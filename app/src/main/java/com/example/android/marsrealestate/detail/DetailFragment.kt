4fd0adda21e427e95d10ae74324e4748a93b0341
AdriMillares <a01705674@itesm.mx>
2021-11-01 16:09:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2019, The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.marsrealestate.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.marsrealestate.databinding.FragmentDetailBinding
+
+/**
+ * Despliega la información de la parcelas de Marte seleccionada
+ * Obtiene la información [DetailViewModel]
+ * @param marsProperty Detalle de la propiedad
+ * @param viewModelFactory Obtiene los datos
+ * @return la vista con la informacion obtenida
+ */
+class DetailFragment : Fragment() {
+
+    /**
+     * Se ejecuta cuando la vista ya está crada
+     * Despliega la información de la parcelas de Marte seleccionada
+     * Obtiene la información [DetailViewModel]
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+
+        val application = requireNotNull(activity).application
+        val binding = FragmentDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+        binding.viewModel = ViewModelProvider(
+                this, viewModelFactory).get(DetailViewModel::class.java)
+
+        return binding.root
+    }
+}
